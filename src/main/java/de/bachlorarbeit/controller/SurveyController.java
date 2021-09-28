730c33ca875ce3942a52c042a5d4100fa7107d58
Luzia <luzia.klauser@stud.uni-regensburg.de>
2021-09-28 12:58:41 +0200

    Finall survey.html and survey.js for dynamic survey, each question has to be answered

--- src/main/java/de/bachlorarbeit/controller/SurveyController.java
+++ src/main/java/de/bachlorarbeit/controller/SurveyController.java
@@ -4,13 +4,18 @@ import com.sun.istack.NotNull;
 import de.bachlorarbeit.service.DatabaseService;
 import de.bachlorarbeit.service.SurveyService;
 import org.json.simple.JSONObject;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -34,5 +39,14 @@ public class SurveyController {
         return null;
     }
 
+    @RequestMapping(value = "/survey/upload",
+            produces = {MediaType.APPLICATION_JSON_VALUE, "application/vnd.pfc-v1.0+json"},
+            method = RequestMethod.POST)
+    public ResponseEntity<?> uploadFiles(@RequestParam HashMap<String, Object> formData) {
+        System.out.println(formData);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body("hello");
+    }
 }
 
--- src/main/resources/static/survey.js
+++ src/main/resources/static/survey.js
@@ -1,73 +1,4 @@
 
-function functionData(data) {
-    var myBooks = data;
-    // EXTRACT VALUE FOR HTML HEADER.
-    // ('Book ID', 'Book Name', 'Category' and 'Price')
-    var col = [];
-    for (var i = 0; i < myBooks.length; i++) {
-        for (var key in myBooks[i]) {
-            if (col.indexOf(key) === -1) {
-                col.push(key);
-            }
-        }
-    }
-    // CREATE DYNAMIC TABLE.
-    var table = document.createElement("table");
-
-    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-
-    var tr = table.insertRow(-1);                   // TABLE ROW.
-
-    for (var i = 0; i < col.length; i++) {
-        var th = document.createElement("th");      // TABLE HEADER.
-        th.innerHTML = col[i];
-        tr.appendChild(th);
-    }
-
-    // ADD JSON DATA TO THE TABLE AS ROWS.
-    for (var i = 0; i < myBooks.length; i++) {
-
-        tr = table.insertRow(-1);
-
-        for (var j = 0; j < col.length; j++) {
-            var tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = myBooks[i][col[j]];
-        }
-    }
-
-    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-    var divContainer = document.getElementById("showSurvey");
-    console.log(divContainer);
-    divContainer.innerHTML = "";
-    divContainer.appendChild(table);
-}
-function createTable(){
-    $.ajax({
-        type: "GET",
-        url: '/data/indicator',
-        success: function (data) {
-            // Call this function on success
-            functionData(data);
-            return data;
-        },
-        error: function () {
-            alert('Error occured');
-        }
-    });
-}
-
-function getData (tableName, callback){
-    $.ajax({
-        url: "/data/enabler",
-        method: "GET",
-        dataType: "json",
-        error: function (){
-            alert('Error occured');
-        },
-    })
-        .done(callback);
-}
-
 function createSurvey() {
     $.ajax({
         type: "GET",
@@ -88,8 +19,10 @@ function createForm(data){
     var questions = data;
     var form = document.createElement("form");
     form.setAttribute("method", "post");
-    form.setAttribute("action", "submit.php");
+    form.setAttribute("id", "surveyForm");
     form.setAttribute("enctype", "multipart/form-data");
+    form.setAttribute("action", "/survey/upload");
+
 
     var card = document.createElement("div");
     card.setAttribute("class", "card");
@@ -110,12 +43,14 @@ function createForm(data){
         cardBody.setAttribute("class", "card-body");
         //Input Yes
         var inputYes = document.createElement("input");
+        inputYes.required=true;
         inputYes.setAttribute("type", "radio")
         inputYes.setAttribute("name", id)
         inputYes.setAttribute("id", "yes"+id);
+        inputYes.setAttribute("value", "yes");
         var labelYes =document.createElement("label");
         labelYes.setAttribute("for", "yes"+id);
-        labelYes.innerHTML="YES  ";
+        labelYes.innerHTML="YES";
 
         //ADD Input, LAbel Yes to card-body
         cardBody.appendChild(inputYes);
@@ -126,6 +61,7 @@ function createForm(data){
         inputNo.setAttribute("type", "radio");
         inputNo.setAttribute("name", id);
         inputNo.setAttribute("id", "no"+id);
+        inputNo.setAttribute("value", "no");
         var labelNo =document.createElement("label");
         labelNo.setAttribute("for", "no"+id);
         labelNo.innerHTML="NO";
@@ -135,21 +71,16 @@ function createForm(data){
         cardBody.appendChild(labelNo);
 
         card.appendChild(cardBody);
-
-
     }
-
     // create a submit button
     var s = document.createElement("input");
     s.setAttribute("type", "submit");
     s.setAttribute("value", "Submit");
+    s.setAttribute("id", "submitUpload");
     form.appendChild(card);
     form.appendChild(s);
 
-    <button class="btn btn-primary" type="submit" id="submitUpload">Upload</button>
     var divContainer = document.getElementById("survey");
     divContainer.appendChild(form);
-
-
 }
 
